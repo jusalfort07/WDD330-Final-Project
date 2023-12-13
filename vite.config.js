f9a88cee944f62dbadca4c35529515c2935a921d
Alfred Ortega <jusalfort.07@gmail.com>
2023-12-13 21:08:27 +0800

    [feat] Home: create Home Page

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        contact: resolve(__dirname, "src/contact/index.html"),
         signup: resolve(__dirname, "src/signup/index.html"),
         signup_success: resolve(__dirname, "src/signup_success/index.html"),
         pet_pages: resolve(__dirname, "src/pet_pages/index.html"),
